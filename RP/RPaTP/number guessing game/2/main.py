a29720c1defef51f29e13cdcb95cefc7152b05e4
DFcreationn <diheinfernando954@gmail.com>
2024-01-02 19:36:01 +0100

    v 0.02

---
+++ RP/RPaTP/number guessing game/2/main.py
@@ -0,0 +1,31 @@
+from random import randrange as rn
+import random as rm
+from time import sleep as sl
+import time as tm
+
+
+
+class Var:
+    rnctp = rn(1, 1000000)
+    p1 = "Welcome to the number guessing game"
+    p2 = "How to play?"
+    p3 = "First the program will choose a number between 1 and 1000000 and you have to guess what is the number."
+    ip1 = "Choose your number"
+    si1 = 0
+    p4 = "You guessed the number !!!"
+    p5 = "The number you choosed was wrong retry :/"
+    t = 0
+    p6 = "It took you "
+    p7 = " tries to guess the number"
+
+
+def introduction():
+    print(Var.p1)
+    sl(2.5)
+    print(Var.p2)
+    sl(2.5)
+    print(Var.p3)
+    Var.si1 = 1
+
+def citnmtnttphittp():
+    introduction()
